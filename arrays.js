e9e71beae8a1dd1d5d274eae672192d7ca1b54e5
Liz Burton <liz.burton147@gmail.com>
2017-10-27 16:59:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,8 +11,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-//  return array.unshift(element)
-    return array=[element, ...array]
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element) {
